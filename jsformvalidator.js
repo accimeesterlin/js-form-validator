d0c93f054c659a1e0afd6c9756d01db2384a60b8
lynda <lyndachiwe@gmail.com>
2015-11-01 19:35:40 +0100

    remove unnecessary comments

--- jsformvalidator.js
+++ jsformvalidator.js
@@ -131,22 +131,6 @@ $(document).ready(function(){
 			var currentform = $(this).parents('form:first');
 			var className = '.validate-error-'+nameField;
 
-			// if(req && req == "on"){
-			// var valid = validateRequired($(this));
-
-
-			// if(valid._error)
-			// {
-			// 	currentform.find(className).html(valid._error);
-			// 	//e.preventDefault();
-			// 	errorObj.push(_error);
-			// }
-			// else
-			// 	currentform.find(className).html("");
-
-			// }
-
-			// else{
 
 			var v = myValidator($(this),req);
 
@@ -166,7 +150,6 @@ $(document).ready(function(){
 
 			}
 
-		//}
 	});
 	
 		console.log(errorObj)
