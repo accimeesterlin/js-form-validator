cbeac6084a08ebee66ff576f799c9b5feb8f7b96
lynda <lyndachiwe@gmail.com>
2015-11-01 16:19:15 +0100

    url validator, regex

--- jsformvalidator.js
+++ jsformvalidator.js
@@ -42,7 +42,11 @@ var requiredError = "This field is required";
 	function validateUrlOnly(field)
 		{
 			var value = field.val();
-		
+			var myRegExp =/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+			if (!myRegExp.test(value))
+			return false;
+			else
+			return true;	
 
 		}
 
@@ -94,7 +98,7 @@ $(document).ready(function(){
 	$('.validate-this-form').each(function(){
 		var validationtype = $(this).attr('vtype');
 		$(this).find('input').each(function(){
-			alert($(this).attr('name'));
+			//alert($(this).attr('name'));
 			
 			var v = myValidator($(this));
 			console.log("v====>",v)
